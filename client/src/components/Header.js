aec93f00e0c920fe11f591fdcab95094900094f3
Jeremiah Arellano <churellano@gmail.com>
2019-11-30 16:54:59 -0800

    Change maps to map

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -11,11 +11,7 @@ const Header = () => (
       <ul>
         <li><Link to='/'>Home</Link></li>
         <li><Link to='/profile'>Profile</Link></li>
-<<<<<<< HEAD
         <li><Link to='/map'>Map</Link></li>
-=======
-        <li><Link to='/maps'>Maps</Link></li>
->>>>>>> 0ebd6dbfc62717a2d56d2228b45426fe8b1f19ae
       </ul>
     </nav>
   </header>
