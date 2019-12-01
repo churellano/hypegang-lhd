f1cac61fa985b1088052f03a26b8480fbb5b6912
Guanting <16443280+baconandchips@users.noreply.github.com>
2019-11-30 19:42:18 -0800

    push thing

--- client/src/components/Safehaven.js
+++ client/src/components/Safehaven.js
@@ -2,8 +2,57 @@ import React from 'react'
 
 const Safehaven = () => (
   <div>
-    <h1>Choose Space for Map</h1>
-  </div>
+    <section className="resume-section p-3 p-lg-5 d-flex justify-content-center" id="experience">
+      <div className="w-100">
+        <h2 className="mb-5">History</h2>
+        <div className="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
+          <div className="resume-content">
+            <h3 className="mb-0">Koerner Library</h3>
+            <div className="subheading mb-3">Library</div>
+            <p>Quiet library in the center of campus, Koerner has a history of sheltering students who are Irving sound refugees. Pretty busy during finals, but usually pretty empty in the beginning of the semester. </p>
+
+        <div className="social-icons">
+          <a href="#">
+        
+            Peanut Allergy
+          </a>
+          <br />
+          <a href="#">
+            
+            Wheelchair Friendly
+          </a>
+        </div>
+          </div>
+
+          <div className="resume-date text-md-right">
+            <span className="text-primary">March 2013 - Present</span>
+          </div>
+          
+        </div>
+
+
+        <div className="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
+          <div className="resume-content">
+            <h3 className="mb-0">Wreck Beach</h3>
+            <div className="subheading mb-3">Beach</div>
+            <p>It's nuts!</p>
+              <div className="social-icons">
+          <a href="#">
+            
+            Peanut Allergy
+          </a>
+        </div>
+          </div>
+          </div>
+          <div className="resume-date text-md-right">
+            <span className="text-primary">July 2010 - December 2011</span>
+          </div>
+        </div>
+        </section>
+      </div>
+
+    
+ 
 )
 
 export default Safehaven
