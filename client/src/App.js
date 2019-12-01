d5bb03160220abf3a049757dfef3f486fcd2155b
Guanting <16443280+baconandchips@users.noreply.github.com>
2019-11-30 16:22:39 -0800

    Middle of idea - not compiling

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,12 @@ import './App.css';
 
 function App() {
   return (
+    <div>
+      <Header />
+      <Main />
+    </div>
+  );
+  /*
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -20,7 +26,7 @@ function App() {
         </a>
       </header>
     </div>
-  );
+    */
 }
 
 export default App;
