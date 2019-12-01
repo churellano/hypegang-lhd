9e9e76a333ab52367c53943a4c7073dcd46ccd46
Jeremiah Arellano <churellano@gmail.com>
2019-11-30 16:32:14 -0800

    Make base code compile

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from "./components/Header"
+import Main from "./components/Main"
 
 function App() {
   return (
@@ -9,24 +10,6 @@ function App() {
       <Main />
     </div>
   );
-  /*
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-    */
 }
 
 export default App;
